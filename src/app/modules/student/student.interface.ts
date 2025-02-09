1626e2848a979585a1cc8b4383771da2a5285a14
Kashif <krreza200@gmail.com>
2025-02-09 23:07:56 +0600

    Interface and schema

---
+++ src/app/modules/student/student.interface.ts
@@ -0,0 +1,37 @@
+export type Guardian = {
+  fatherName: string;
+  fatherOccupation: string;
+  fatherContactName: string;
+  motherName: string;
+  motherOccupation: string;
+  motherContactName: string;
+};
+
+export type UserName = {
+  firstName: string;
+  middleName: string;
+  lastName: string;
+};
+export type LocalGaurdian = {
+  name: string;
+  occupation: string;
+  contactNo: string;
+  address: string;
+};
+
+export type Student = {
+  id: string;
+  name: UserName;
+  gender: 'male' | 'female';
+  dateOfBirth: string;
+  email: string;
+  contactNo: string;
+  emergencyContactNo: string;
+  bloodGroup?: 'A+' | 'A-' | 'B+' | 'B-' | 'o+' | 'o-' | 'AB+' | 'AB-';
+  presentAddress: string;
+  permanentAddress: string;
+  guardian: Guardian;
+  localGaurdian: LocalGaurdian;
+  profileImg?: string;
+  isActive: 'active' | 'inactive';
+};
